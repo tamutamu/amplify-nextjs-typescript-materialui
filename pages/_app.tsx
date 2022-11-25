27405aa69316c2d2c43ebb3dd59fa27030736b26
tamutamu <tamu.0.0.tamu@gmail.com>
2022-11-25 20:34:30 +0900

    Init.

---
+++ pages/_app.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head"
+import { AppProps } from "next/app"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import theme from "~/mui/theme"
+import createEmotionCache from "~/mui/createEmotionCache"
+import { Authenticator } from "@aws-amplify/ui-react"
+import "@aws-amplify/ui-react/styles.css"
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+type MyAppProps = AppProps & {
+  emotionCache?: EmotionCache
+}
+
+const MyApp = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Authenticator>
+          <Component {...pageProps} />
+        </Authenticator>
+      </ThemeProvider>
+    </CacheProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+import type { NextPage } from "next"
+
+const Home: NextPage = () => {
+  return (
+    <div css={{ padding: "10px" }}>
+      <Button variant="contained">Hello, World!</Button>
+    </div>
+  )
+}
+
+export default Home
